638e78dd9b5b6c6062d785a6a181d6c5557703df
Joao Vitor <joaovsantos.dev@gmail.com>
2025-01-29 14:22:25 -0300

    Added the fixed property to the menu when scrolling vertically and implemented interaction between the menu and the page sections.

---
+++ script.js
@@ -0,0 +1,32 @@
+function initNavMenu() {
+  const menuNav = document.querySelectorAll('.menu a[href^="#"]');
+
+  function addScrollSmooth(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    console.log(topo);
+    
+    window.scrollTo({
+      top: topo - 90,
+      behavior: "smooth",
+    })
+  }
+
+  menuNav.forEach((item) => {
+    item.addEventListener("click", addScrollSmooth);
+  });
+}
+initNavMenu();
+
+window.addEventListener("scroll", () => {
+  const menu = document.querySelector(".menu-items"); // Substitua pelo ID ou classe do seu menu
+  const scrollTop = window.scrollY;
+
+  if (scrollTop >= 90) {
+      menu.classList.add("fixed-menu");
+  } else{
+      menu.classList.remove("fixed-menu");
+  }
+});
